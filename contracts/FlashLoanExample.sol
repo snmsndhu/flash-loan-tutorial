f3190c905cc499879861f236e0414110439e44c1
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-12-15 07:39:58 -0500

    FlashLoan-Example

--- contracts/FlashLoanExample.sol
+++ contracts/FlashLoanExample.sol
@@ -7,5 +7,11 @@ import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract FlashLoanExample is FlashLoanSimpleReceiverBase {
+    using SafeMath for uint;
+    event log (address asset, uint val);
+
+    constructor(IPoolAddressesProvider provider) FlashLoanSimpleReceiverBase(provider){}
+
     
+
 }
