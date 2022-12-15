7900da5b8c1ba98fcce33f1daaa7fdb30a7682ed
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-12-15 08:00:34 -0500

    FlashLoan-Example

--- contracts/FlashLoanExample.sol
+++ contracts/FlashLoanExample.sol
@@ -1,42 +1,46 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+
 contract FlashLoanExample is FlashLoanSimpleReceiverBase {
-    using SafeMath for uint;
-    event log (address asset, uint val);
+  using SafeMath for uint;
+  event Log(address asset, uint val);
 
-    constructor(IPoolAddressesProvider provider) FlashLoanSimpleReceiverBase(provider){}
+  constructor(IPoolAddressesProvider provider)
+    FlashLoanSimpleReceiverBase(provider)
+  {}
+  
+  function createFlashLoan(address asset, uint amount) external {
+      address receiver = address(this);
+      bytes memory params = ""; // use this to pass arbitrary data to executeOperation
+      uint16 referralCode = 0;
 
-    function createFlashLoan(address asset, uint amount) external {
-        address receiver = address(this);
-        bytes memory params = "";
-        uint16 referralCode = 0 ;
-
-        Pool.flashLoanSimple(
+      POOL.flashLoanSimple(
        receiver,
        asset,
        amount,
        params,
        referralCode
-        );
-    }
+      );
+  }
 
-    function executeOperation(
+   function executeOperation(
     address asset,
     uint256 amount,
     uint256 premium,
     address initiator,
-    bytes calldata params) external returns(bool) {
+    bytes calldata params
+  ) external returns (bool){
+    // do things like arbitrage here
+    // abi.decode(params) to decode params
     
     uint amountOwing = amount.add(premium);
     IERC20(asset).approve(address(POOL), amountOwing);
     emit Log(asset, amountOwing);
     return true;
   }
-    }
 }
